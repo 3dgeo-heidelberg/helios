80794979cd7faeb68167b2f629970c39410d558c
Miguel Yermo <miguel.yermo@uni-heidelberg.de>
2021-07-01 18:55:48 +0200

    Added WavefrontObjCache destructor.

--- src/assetloading/geometryfilter/WavefrontObjCache.cpp
+++ src/assetloading/geometryfilter/WavefrontObjCache.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "WavefrontObjCache.h"
+
+WavefrontObjCache &WavefrontObjCache::getInstance() {
+  static WavefrontObjCache instance;
+  return instance;
+}
+
+void WavefrontObjCache::saveScenePart(const std::string &key, ScenePart *sp) {
+  cache[key] = new ScenePart(*sp);
+}
+
+ScenePart *WavefrontObjCache::get(const std::string &key) {
+  return cache.at(key);
+}
+
+bool WavefrontObjCache::exists(const std::string &key) {
+  return !(cache.find(key) == cache.end());
+}
+
+WavefrontObjCache::~WavefrontObjCache() {
+  for (auto &object : cache)
+    delete object.second;
+}
--- src/assetloading/geometryfilter/WavefrontObjCache.h
+++ src/assetloading/geometryfilter/WavefrontObjCache.h
@@ -8,46 +8,58 @@
 #include "ScenePart.h"
 #include <unordered_map>
 
-class WavefrontObjCache
-{
+/**
+ * @brief Class representing a cache for all the .obj loaded models
+ */
+class WavefrontObjCache {
 private:
+  // ***  ATTRIBUTES  *** //
+  // ******************** //
+  /**
+   * @brief Map where the loaded sceneparts will be stored
+   */
+  std::unordered_map<std::string, ScenePart *> cache{};
+
+  // ***  CONSTRUCTION / DESTRUCTION  *** //
+  // ************************************ //
+  /**
+   * @brief Default constructor for a default OBJ Cache
+   */
   WavefrontObjCache() = default;
-
-  std::unordered_map<std::string, ScenePart*> cache{};
-
-public:
-  static WavefrontObjCache & getInstance()
-  {
-    static WavefrontObjCache instance;
-    return instance;
-  }
-
-  void saveScenePart(const std::string & key, ScenePart* sp)
-  {
-    cache[key] = new ScenePart(*sp);
-  }
-
-  ScenePart * loadScenePart(const std::string & key)
-  {
-    // TODO: Hacer copia de miembros, no alojar de nuevo!
-    return new ScenePart(*cache[key]);
-  }
-
-  ScenePart * get(const std::string & key)
-  {
-    return cache.at(key);
-  }
-
-  bool exists(const std::string & key)
-  {
-    return !(cache.find(key) == cache.end());
-  }
+  ~WavefrontObjCache();
 
 public:
+  // *** M E T H O D S *** //
+  // **********************//
+  /**
+   * @brief Get the caché object itself. This functions returns always
+   * the same instance of the class
+   * @return Instance of WavefrontObjCache
+   */
+  static WavefrontObjCache &getInstance();
+  /**
+   * @brief Saves an ScenePart using the loaded namefile as a key
+   * @param key Key where the ScenePart will be stored
+   * @param sp The ScenePart itself
+   */
+  void saveScenePart(const std::string &key, ScenePart *sp);
+  /**
+   * @brief Returns a ScenePart stored in key
+   * @param key Key where the ScenePart to be loaded is located
+   * @return A pointer to a stored ScenePart
+   */
+  ScenePart *get(const std::string &key);
+  /**
+   * @brief Checks if a key is already stored in the cache
+   * @param key
+   * @return
+   */
+  bool exists(const std::string &key);
+
   // Avoid accidental copies of this class
-  WavefrontObjCache(WavefrontObjCache const&) = delete;  // Don't Implement
-  void operator=(WavefrontObjCache const&) = delete;     // Don't implement
-
+  WavefrontObjCache(WavefrontObjCache const &) = delete; // Don't Implement
+  WavefrontObjCache
+  operator=(WavefrontObjCache const &) = delete; // Don't implement
 };
 
 #endif // HELIOS_WAVEFRONTOBJCACHE_H
