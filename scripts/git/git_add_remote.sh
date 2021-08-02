f3eddce9c145fa88c617e2c84cf0db6ffd6231d7
uadmin <albertoesmp@gmail.com>
2021-08-02 18:38:25 +0200

    Removed local scripts. No interest for pull request

--- scripts/git/git_add_remote.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Author : Alberto M. Esmoris Pena
-#
-# Script to add helios as remote for the fork
-
-git remote add heliospp https://github.com/3dgeo-heidelberg/helios.git
--- scripts/git/git_update_remote.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Author : Alberto M. Esmoris Pena
-#
-# Script to synchronize the fork with source helios remote
-# NOTICE this scripts requires remote has been added before using
-# the git_add_remove.sh script
-
-git fetch heliospp
