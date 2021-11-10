b381dcaf77c1fdf5e6f26a19ad89342f662edf04
Hannah Weiser <h.weiser@stud.uni-heidelberg.de>
2021-11-10 07:55:24 +0100

    removed lines

--- extra/xmlchecker.py
+++ extra/xmlchecker.py
@@ -4,5 +4,3 @@ xmlschema.validate(r'..\data\surveys\demo\tls_arbaro_demo.xml', 'survey.xsd')
 xmlschema.validate(r'..\data\scenes\demo\arbaro_demo.xml', 'scene.xsd')
 xmlschema.validate(r'..\data\scanners_als.xml', 'scanner.xsd')
 xmlschema.validate(r'..\data\platforms.xml', 'platform.xsd')
-
-
