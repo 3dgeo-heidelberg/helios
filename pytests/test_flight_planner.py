832fb3c141ee1017b9e6dec53fecefb74f2069de
Hannah Weiser <h.weiser@stud.uni-heidelberg.de>
2021-12-07 16:13:37 +0100

    added more flight_planner tests

--- pytests/test_flight_planner.py
+++ pytests/test_flight_planner.py
@@ -6,6 +6,7 @@ import numpy as np
 from pathlib import Path
 import sys
 from extra import flight_planner
+import re
 
 # HELIOS_EXE = str(Path('build') / 'helios')
 # if sys.platform == "win32":
@@ -71,38 +72,132 @@ def test_export_for_xml_not_always_active():
                           [5, 10],
                           [5, 0]])
     altitude = 200
-    id = "template"
+    template_id = "template"
     speed = 50
     expected_legs_xml = f'''
         <leg>
             <platformSettings x="{waypoints[0, 0]}" y="{waypoints[0, 1]}" z="{altitude}" movePerSec_m="{speed}" />
-            <scannerSettings template="{id}" trajectoryTimeInterval_s="0.05" />
+            <scannerSettings template="{template_id}" trajectoryTimeInterval_s="0.05" />
         </leg>
         
         <leg>
             <platformSettings x="{waypoints[1, 0]}" y="{waypoints[1, 1]}" z="{altitude}" movePerSec_m="{speed}" />
-            <scannerSettings template="{id}" active="false" trajectoryTimeInterval_s="0.05" />
+            <scannerSettings template="{template_id}" active="false" trajectoryTimeInterval_s="0.05" />
         </leg>
         
         <leg>
             <platformSettings x="{waypoints[2, 0]}" y="{waypoints[2, 1]}" z="{altitude}" movePerSec_m="{speed}" />
-            <scannerSettings template="{id}" trajectoryTimeInterval_s="0.05" />
+            <scannerSettings template="{template_id}" trajectoryTimeInterval_s="0.05" />
         </leg>
         
         <leg>
             <platformSettings x="{waypoints[3, 0]}" y="{waypoints[3, 1]}" z="{altitude}" movePerSec_m="{speed}" />
-            <scannerSettings template="{id}" active="false" trajectoryTimeInterval_s="0.05" />
+            <scannerSettings template="{template_id}" active="false" trajectoryTimeInterval_s="0.05" />
         </leg>
         '''
 
-    legs_xml = flight_planner.export_for_xml(waypoints, altitude, id, speed)
+    legs_xml = flight_planner.export_for_xml(waypoints, altitude, template_id, speed)
 
     assert legs_xml == expected_legs_xml
 
 
 def test_add_translation_filter():
-    pass
+    """Function to test function add_transformation_filters for a translation"""
+    # given
+    tr = [10, 10, 5]
+
+    # expected:
+    trafo_filter_expected = f"""
+            <filter type="translate">  
+                <param type="integer" key="onGround" value="0" />
+                <param type="vec3" key="offset" value="{tr[0]};{tr[1]};{tr[2]}" /> 
+            </filter>\n"""
+
+    trafo_filter = flight_planner.add_transformation_filters(translation=tr)
+
+    assert re.sub(r"\s+", " ", trafo_filter) == re.sub(r"\s+", " ", trafo_filter_expected)
 
 
-def test_create_scenepart_obj():
-    pass
+def test_add_rotation_filter():
+    """Function to test function add_transformation_filters for a rotation"""
+    # given
+    rot = [90, 0, 180]
+
+    # expected:
+    rot_filter_expected = f"""
+            <filter type="rotate">
+                <param key="rotation" type="rotation">  
+                    <rot angle_deg="{rot[0]}" axis="x"/>  
+                    <rot angle_deg="{rot[1]}" axis="y"/>  
+                    <rot angle_deg="{rot[2]}" axis="z"/>  
+                </param>
+            </filter>\n"""
+
+    rot_filter = flight_planner.add_transformation_filters(rotation=rot)
+
+    assert re.sub(r"\s+", " ", rot_filter) == re.sub(r"\s+", " ", rot_filter_expected)
+
+
+def test_add_scale_filter():
+    """Function to test function add_transformation_filters for a translation"""
+    # given
+    sc = 0.5
+
+    # expected:
+    scale_filter_expected = f"""
+            <filter type="scale">
+                <param type="double" key="scale" value="{sc}" />
+            </filter>\n"""
+
+    scale_filter = flight_planner.add_transformation_filters(scale=sc)
+
+    assert re.sub(r"\s+", " ", scale_filter) == re.sub(r"\s+", " ", scale_filter_expected)
+
+
+def test_add_translation_filter_on_ground():
+    """Function to test function add_transformation_filters for a translation onto ground"""
+    # given
+    tr = [10, 10, 5]
+    on_gnd = -1
+
+    # expected:
+    trafo_filter_expected = f"""
+                    <filter type="translate">  
+                        <param type="integer" key="onGround" value="{on_gnd}" />
+                        <param type="vec3" key="offset" value="{tr[0]};{tr[1]};{tr[2]}" /> 
+                    </filter>\n"""
+
+    trafo_filter = flight_planner.add_transformation_filters(translation=tr, on_ground=on_gnd)
+
+    assert re.sub(r"\s+", " ", trafo_filter) == re.sub(r"\s+", " ", trafo_filter_expected)
+
+
+def test_create_scenepart_obj_efilepath():
+    # given
+    filepath = "data/sceneparts/basic/groundplane/groundplane.obj"
+
+    # expected
+    obj_filter_expected = f'''
+            <part>
+                <filter type="objloader">
+                    <param type="string" key="efilepath" value="{filepath} up="z" />
+                </filter>
+
+            </part>'''
+
+    obj_filter = flight_planner.create_scenepart_obj(filepath, efilepath=True)
+
+    # raise AssertionError
+    assert re.sub(r"\s+", " ", obj_filter) == re.sub(r"\s+", " ", obj_filter_expected)
+
+
+def test_create_scenepart_obj_invalid_up():
+    # given
+    filepath = "data/sceneparts/basic/groundplane/groundplane.obj"
+    up_axis = "phi"
+
+    # expected
+    # raise AssertionError
+    with pytest.raises(AssertionError) as e:
+        flight_planner.create_scenepart_obj(filepath, up_axis=up_axis)
+    assert e.type is AssertionError
