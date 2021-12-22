f69b562b056ccf938d8b08e40af48d0e58aae92a
lwiniwar <lukas.winiwarter@uni-heidelberg.de>
2021-12-22 09:44:42 +0100

    solved osgeo issue

--- pyhelios/_pyhelios_relay.py
+++ pyhelios/_pyhelios_relay.py
@@ -1,6 +1,9 @@
 import sys
 from pathlib import Path
-
+try:
+    import osgeo  # if osgeo/gdal is installed, import it _before_ importing pyhelios or altering the PATH
+except:
+    pass
 # append run folder to sys path before import
 sys.path.append(str(Path(__file__).parent.parent / "run"))
 from _pyhelios import *
