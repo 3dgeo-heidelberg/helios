e62364ed8ec47a88c6d86474837ced78cfe7ad01
uadmin <albertoesmp@gmail.com>
2022-05-03 22:01:05 +0200

    Updating pytests: Expected output size must be updated for new time resolution (ns)

--- pytests/test_demo_scenes.py
+++ pytests/test_demo_scenes.py
@@ -184,7 +184,7 @@ def eval_interpolated_traj(dirname):
         for _ in range(3):
             next(f)
         line = f.readline()
-        assert line.startswith('13.4766 1.7424 400.0000')
+        assert line.startswith('13.4764 1.7423 400.0000')
     # clean up
     if DELETE_FILES_AFTER:
         shutil.rmtree(dirname)
