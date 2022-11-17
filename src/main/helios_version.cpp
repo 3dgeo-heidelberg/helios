136f210ca4d3aaa8428bede4b1df24ccb164ec0a
uadmin <albertoesmp@gmail.com>
2022-11-17 16:59:05 +0100

    LAS1.4 works also for SingleScanner

--- src/main/helios_version.cpp
+++ src/main/helios_version.cpp
@@ -4,7 +4,7 @@
 
 const char * HELIOS_VERSION = "1.1.1";
 
-const char * HELIOS_GIT_HASH = "b1de8cfd";
+const char * HELIOS_GIT_HASH = "7dfe61d3";
 
 const char * getHeliosVersion(){
     return HELIOS_VERSION;
