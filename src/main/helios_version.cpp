64868c7898ab7b2a688d2e31ebcf76d07f463036
han16nah <han_nah@mailbox.org>
2023-02-23 23:42:16 +0100

    bump version - ready for release

--- src/main/helios_version.cpp
+++ src/main/helios_version.cpp
@@ -2,9 +2,9 @@
 
 #include <sstream>
 
-const char * HELIOS_VERSION = "1.1.1";
+const char * HELIOS_VERSION = "1.2.0";
 
-const char * HELIOS_GIT_HASH = "1ce4586a";
+const char * HELIOS_GIT_HASH = "54951087";
 
 const char * getHeliosVersion(){
     return HELIOS_VERSION;
