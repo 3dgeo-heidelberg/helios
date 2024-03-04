30c5e7889445914e95ddc5161ca8a39538439c6f
albertoesmp <albertoesmp@gmail.com>
2024-03-04 16:54:46 +0100

    Solving hang-up after end of simulation

--- src/main/helios_version.cpp
+++ src/main/helios_version.cpp
@@ -4,7 +4,7 @@
 
 const char * HELIOS_VERSION = "1.3.0";
 
-const char * HELIOS_GIT_HASH = "31904380";
+const char * HELIOS_GIT_HASH = "cefd1fce";
 
 const char * getHeliosVersion(){
     return HELIOS_VERSION;
