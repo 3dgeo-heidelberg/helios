f2bdaf508e9020bac176b947902951ad6b65d936
albertoesmp <albertoesmp@gmail.com>
2024-05-29 10:42:39 +0200

    Solved issue with simulation shutdown in PyHelios

--- src/pybinds/PyHeliosSimulation.cpp
+++ src/pybinds/PyHeliosSimulation.cpp
@@ -59,6 +59,10 @@ PyHeliosSimulation::PyHeliosSimulation(
     xmlreader = std::make_shared<XmlSurveyLoader>(surveyPath, assetsPath);
 }
 PyHeliosSimulation::~PyHeliosSimulation() {
+    if(playback != nullptr){
+        playback->mSurvey->scanner->getDetector()->shutdown();
+        playback->mSurvey->scanner->platform->scene->shutdown();
+    }
     if(thread != nullptr) delete thread;
 }
 
@@ -150,7 +154,8 @@ void PyHeliosSimulation::start (){
         chunkSize,
         fixedGpsTimeStart,
         legacyEnergyModel,
-        exportToFile
+        exportToFile,
+        true
     );
     playback->callback = callback;
     playback->setCallbackFrequency(callbackFrequency);
