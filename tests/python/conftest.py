5cceb5f0d15a07f57b634ac796c99762966d0bdd
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2025-02-14 13:45:38 +0100

    Port regression testing setup

--- tests/python/conftest.py
+++ tests/python/conftest.py
@@ -16,7 +16,49 @@ from helios.survey import Survey
 from helios.util import set_rng_seed
 
 import math
+import os
+import pathlib
+import pooch
 import pytest
+
+
+# Regression data archives to download
+TEST_DATA_ARCHIVE = "https://github.com/3dgeo-heidelberg/helios-test-data/releases/download/2025-02-05/data.tar.gz"
+TEST_DATA_CHECKSUM = "581b9f13ab3dcaf0422a8ab6069ba4534db276c75c098da23dd8ed264a5e7cee"
+
+
+@pytest.fixture
+def regression_data(request):
+    """A fixture that ensures the existence of regression data
+
+    Returns the Path to where that data is located after (cached) download
+    from GitHub.
+    """
+
+    if not request.config.getoption("--regression-tests"):
+        return None
+
+    # Define the cache location
+    cache = pooch.os_cache("helios")
+
+    # Trigger the download
+    pooch.retrieve(
+        TEST_DATA_ARCHIVE,
+        TEST_DATA_CHECKSUM,
+        path=cache,
+        downloader=pooch.HTTPDownloader(timeout=(3, None)),
+        processor=pooch.Untar(extract_dir="."),
+    )
+
+    return cache
+
+
+@pytest.fixture(scope="session")
+def persisting_output_dir(request):
+    if request.config.getoption("--keep-output"):
+        return pathlib.Path(os.getcwd()) / "pytest-output"
+    else:
+        return request.getfixturevalue("tmp_path_factory").mktemp("output")
 
 
 @pytest.fixture(autouse=True)
@@ -106,3 +148,36 @@ def tls_survey(tls_scanner, tripod, scene):
 
 
 survey = tls_survey
+
+
+def pytest_addoption(parser):
+    # Add an option to also run slow tests which are omitted by default
+    parser.addoption(
+        "--slow", action="store_true", default=False, help="run slow tests"
+    )
+
+    # Add an option for running regression tests. This is useful to e.g.
+    # restrict the regression testing bit to the platform on which we have
+    # regression data available.
+    parser.addoption(
+        "--regression-tests",
+        action="store_true",
+        default=False,
+        help="run regression tests",
+    )
+
+    # Add an option to keep the output of the demo regression tests in
+    # the pytest-output folder
+    parser.addoption(
+        "--keep-output",
+        action="store_true",
+        default=False,
+        help="keep output of demo tests",
+    )
+
+
+def pytest_runtest_setup(item):
+    if "slow" in item.keywords and not (
+        item.config.getoption("--slow") or item.config.getoption("--regression-tests")
+    ):
+        pytest.skip("need --slow option to run")
