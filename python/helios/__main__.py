d918d7a068fe881f1be7dfa42948c44472436333
Kai Riedmiller <kai.riedmiller@web.de>
2025-02-25 14:17:23 +0100

    implement survey call from cli

--- python/helios/__main__.py
+++ python/helios/__main__.py
@@ -1,35 +1,9 @@
 import click
 from click_option_group import optgroup, MutuallyExclusiveOptionGroup
-import importlib_resources as resources
-import os
-import subprocess
 import sys
-
-
-def _get_executable():
-    """Locate the compiled Helios executable."""
-    return resources.files("_helios") / "helios" / "bin" / "helios++"
-
-
-def helios_exec(args):
-    #
-    # Inject additional arguments to account for standard paths
-    #
-
-    # We always look for assets in the current working directory
-    args = args + ["--assets", os.getcwd()]
-
-    # We always look in the Python installation tree
-    args = args + ["--assets", resources.files("helios")]
-    args = args + ["--assets", resources.files("helios") / "data"]
-
-    # Inject the legacy model switch. This is part of our transitioning strategy
-    # to the new energy model.
-    args = args + ["--legacyEnergyModel"]
-
-    # Call the executable
-    executable = _get_executable()
-    return subprocess.call([executable] + args)
+from helios.settings import ExecutionSettings, OutputSettings
+from helios.survey import Survey
+from helios.utils import add_asset_directory, set_rng_seed
 
 
 def helios_entrypoint():
@@ -38,17 +12,13 @@ def helios_entrypoint():
 
 
 @click.command()
+@click.argument("survey_file_path", required=True)
 @optgroup.group("Input")
 @optgroup.option(
     "--assets",
     "-a",
     type=click.Path(exists=True, file_okay=False),
     multiple=True,
-    default=(
-        os.getcwd(),
-        # resources.files("helios"),
-        # resources.files("helios") / "data",
-    ),
     help=(
         "Specify the path(s) to assets/data directory. To specify multiple "
         "paths, duplicate the argument,e.g. --assets path1 --assets path2. "
@@ -98,9 +68,15 @@ def helios_entrypoint():
     ),
 )
 @optgroup.option(
+    "--format",
+    default="npy",
+    show_default=True,
+    help="Output format, can be las, laz, xyz, npy, or laspy.",
+)
+@optgroup.option(
     "--lasOutput",
     is_flag=True,
-    help=("Use this flag to generate the output point cloud in LAS format (v 1.4)"),
+    help="Use this flag to generate the output point cloud in LAS format (v 1.4)",
 )
 @optgroup.option(
     "--las10", is_flag=True, help="Use this flag to write in LAS format (v 1.0)"
@@ -134,10 +110,9 @@ def helios_entrypoint():
 )
 @optgroup.option(
     "--seed",
-    type=click.STRING,
+    type=click.INT,
     help=(
-        "Specify the seed for randomness generation. It can be an intenger, "
-        "a decimal or a timestamp with format YYYY-mm-DD HH::MM::SS. "
+        "Specify the seed for randomness generation. It must be an intenger"
         "By default: a random seed is generated."
     ),
 )
@@ -152,12 +127,12 @@ def helios_entrypoint():
 )
 @optgroup.option(
     "--parallelization",
-    type=click.Choice(choices=("chunk", "warehouse")),
-    default="chunk",
+    type=click.INT,
+    default=0,
     show_default=True,
     help=(
-        "Specify the parallelization strategy. 'chunk' for a static/dynamic chunk "
-        "based parallelization and 'warehouse' for a warehouse based one. "
+        "Specify the parallelization strategy. 0 for a static/dynamic chunk "
+        "based parallelization and 1 for a warehouse based one."
     ),
 )
 @optgroup.option(
@@ -165,7 +140,6 @@ def helios_entrypoint():
     "-j",
     "--njobs",
     type=click.INT,
-    default=0,
     show_default=True,
     help=(
         "Specify the number of threads to be used to compute the simulation. "
@@ -227,38 +201,34 @@ def helios_entrypoint():
 @optgroup.group("KDTree")
 @optgroup.option(
     "--kdt",
-    type=click.Choice(choices=("simple", "median", "sah", "sah_approx")),
-    default="sah_approx",
+    type=click.INT,
+    default=4,
     show_default=True,
     help=(
-        "Specify the type of KDTree to be built for for the scene Using 1 is "
-        "for the simple KDTree based on median  balancing, 2 for the SAH "
+        "Specify the type of KDTree to be built for for the scene. Using 1 is "
+        "for the simple KDTree based on median balancing, 2 for the SAH "
         "based KDTree, 3 for the SAH with best axis based KDTree and "
-        "4 (default) for a fast SAH approximation "
+        "4 (default) for a fast SAH approximation."
     ),
 )
 @optgroup.option(
     "--kdtJobs",
     type=click.INT,
-    default=0,
-    show_default=True,
     help=(
         "Specify the number of threads to be used for building the KDTree. "
         "If 1, then the KDTree will be built in a sequential fashion. "
-        "If >1, then the KDTree will be built in a parallel fashion. If 0, "
-        "then the KDTree will be built using as many threads as available."
+        "If >1, then the KDTree will be built in a parallel fashion. By "
+        "default, the KDTree will be built using as many threads as available."
     ),
 )
 @optgroup.option(
     "--kdtGeomJobs",
     type=click.INT,
-    default=0,
-    show_default=True,
     help=(
         "Specify the number of threads to be used for bu upper nodes of the "
         "KDTree (geometry-level parallelization). If 1, then there is no "
         "geometry-level parallelization. If >1, then geometry-level "
-        "parallelization uses as many threads a . If 0 (default), then "
+        "parallelization uses as many threads a . By default "
         "geometry-level parallelization uses as many threads as node-level."
     ),
 )
@@ -308,9 +278,43 @@ def helios_entrypoint():
         "once to report all messages. Default: report errors only."
     ),
 )
-def cli(**kwargs):
-    for k, v in kwargs.items():
-        print(f"{k}: ", v)
+def cli(**kw):
+
+    for k, v in kw.items():
+        print(k, v)
+
+    for asset in kw["assets"]:
+        add_asset_directory(asset)
+
+    if seed := kw.get("seed") is not None:
+        set_rng_seed(seed)
+    else:
+        set_rng_seed()
+
+    output_settings = OutputSettings()
+    execution_settings = ExecutionSettings()
+
+    execution_settings.parallelization = kw.get("parallelization")
+    execution_settings.num_threads = kw.get("num_threads")
+    execution_settings.chunk_size = kw.get("chunksize")
+    execution_settings.warehouse_factor = kw.get("warehousefactor")
+    execution_settings.log_file = kw.get("logfile")
+    execution_settings.verbosity = kw.get("verbose")
+    execution_settings.factory_type = kw.get("kdt")
+    execution_settings.kdt_num_threads = kw.get("kdtjobs")
+    execution_settings.kdt_geom_num_threads = kw.get("kdtgeomjobs")
+    execution_settings.sah_nodes = kw.get("sahnodes")
+
+    output_settings.format = kw.get("format")
+    output_settings.split_by_channel = kw.get("splitbychannel")
+    output_settings.output_dir = kw.get("output")
+    output_settings.write_waveform = kw.get("writewaveform")
+    output_settings.write_pulse = kw.get("writepulse")
+    output_settings.las_scale = kw.get("lasscale")
+
+    survey = Survey.from_xml(kw.get("survey_file_path"))
+
+    survey.run(execution_settings=execution_settings, output_settings=output_settings)
 
 
 if __name__ == "__main__":
