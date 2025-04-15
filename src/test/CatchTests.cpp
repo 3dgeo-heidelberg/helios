8983f5f7eee5c4671d631a346e39eb66526f6f15
Felix <65565033+fexfl@users.noreply.github.com>
2025-04-15 17:51:33 +0200

    added simple test

---
+++ src/test/CatchTests.cpp
@@ -0,0 +1,10 @@
+#include <catch2/catch_test_macros.hpp>
+#include "logging.hpp"
+
+// bool    logging::LOGGING_SHOW_TRACE,    logging::LOGGING_SHOW_DEBUG,
+        // logging::LOGGING_SHOW_INFO,     logging::LOGGING_SHOW_TIME,
+        // logging::LOGGING_SHOW_WARN,     logging::LOGGING_SHOW_ERR;
+
+TEST_CASE( "Test test", "[]" ) {
+    REQUIRE( 1==1 );
+}
