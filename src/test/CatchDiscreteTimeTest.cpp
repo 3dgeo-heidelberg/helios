e71907e42a037aca4cf7db2277fadadd10785c37
Felix <65565033+fexfl@users.noreply.github.com>
2025-04-30 11:59:15 +0200

    Restructured CatchDiscreteTimeTest

--- src/test/CatchDiscreteTimeTest.cpp
+++ src/test/CatchDiscreteTimeTest.cpp
@@ -3,178 +3,182 @@
 #include <catch2/generators/catch_generators.hpp>
 
 TEST_CASE( "Discrete time test" ) {
-    DiscreteTime dt1(500, 1.0);
+    SECTION("Discrete time test with f=500, periodScale=1.0") {
 
-    SECTION("toDiscrete and toContinuous") {
-        auto [cont, disc] = GENERATE(std::make_tuple(0.5, 250), std::make_tuple(1.5, 750));
-        
-        SECTION("toDiscrete") {
-            REQUIRE(dt1.toDiscrete(cont) == disc);
+        DiscreteTime dt1(500, 1.0);
+
+        SECTION("toDiscrete and toContinuous") {
+            auto [cont, disc] = GENERATE(std::make_tuple(0.5, 250), std::make_tuple(1.5, 750));
+            
+            SECTION("toDiscrete") {
+                REQUIRE(dt1.toDiscrete(cont) == disc);
+            }
+            
+            SECTION("toContinuous") {
+                REQUIRE(dt1.toContinuous(disc) == cont);
+            }
         }
-        
-        SECTION("toContinuous") {
-            REQUIRE(dt1.toContinuous(disc) == cont);
+
+        SECTION("toCyclicDiscrete and toPeriodicDiscrete") {
+            auto i = GENERATE(0.5, 1.5, 3.5);
+            
+            SECTION("toCyclicDiscrete") {
+                REQUIRE(dt1.toCyclicDiscrete(i) == 250);
+            }
+            
+            SECTION("toPeriodicDiscrete") {
+                REQUIRE(dt1.toPeriodicDiscrete(i) == 250);
+            }
         }
+
+        SECTION("toCyclicContinuous and toPeriodicContinuous") {
+            auto i = GENERATE(250, 750, 1750);
+
+            SECTION("toCyclicContinuous") {
+                REQUIRE(dt1.toCyclicContinuous(i) == 0.5);
+            }
+
+            SECTION("toPeriodicContinuous") {
+                REQUIRE(dt1.toPeriodicContinuous(i) == 0.5);
+            }
+        }
+
+        SECTION("toDiscrete with small t") {
+            REQUIRE(dt1.toDiscrete(0.00001) == 0);
+        }
+
+        SECTION("toContinuous with 0") {
+            REQUIRE(dt1.toContinuous(0) == 0.0);
+        }
+
+        SECTION("toContinuous with 1") {
+            REQUIRE(dt1.toContinuous(1) > 0.0);
+        }
+
     }
 
-    SECTION("toCyclicDiscrete and toPeriodicDiscrete") {
-        auto i = GENERATE(0.5, 1.5, 3.5);
-        
+    SECTION( "Discrete time test with f=500, periodScale=2.0" ) {
+        DiscreteTime dt2(500, 2.0);
+
+        SECTION("toDiscrete and toContinuous") {
+            auto [cont, disc] = GENERATE(std::make_tuple(0.5, 250), std::make_tuple(1.5, 750));
+            
+            SECTION("toDiscrete") {
+                REQUIRE(dt2.toDiscrete(cont) == disc);
+            }
+            
+            SECTION("toContinuous") {
+                REQUIRE(dt2.toContinuous(disc) == cont);
+            }
+        }
+
         SECTION("toCyclicDiscrete") {
-            REQUIRE(dt1.toCyclicDiscrete(i) == 250);
-        }
-        
-        SECTION("toPeriodicDiscrete") {
-            REQUIRE(dt1.toPeriodicDiscrete(i) == 250);
-        }
-    }
+            auto i = GENERATE(0.5, 1.5, 3.5);
 
-    SECTION("toCyclicContinuous and toPeriodicContinuous") {
-        auto i = GENERATE(250, 750, 1750);
+            REQUIRE(dt2.toCyclicDiscrete(i) == 250);
+        }
+
+        SECTION("toPeriodicDiscrete") {
+            auto [cont, disc] = GENERATE(std::make_tuple(0.0, 0), std::make_tuple(0.2, 100), std::make_tuple(0.5, 250), std::make_tuple(1.5, 750), std::make_tuple(2.0, 0), std::make_tuple(2.2, 100), std::make_tuple(2.5, 250), std::make_tuple(3.5, 750));
+
+            REQUIRE(dt2.toPeriodicDiscrete(cont) == disc);
+        }
 
         SECTION("toCyclicContinuous") {
-            REQUIRE(dt1.toCyclicContinuous(i) == 0.5);
+            auto i = GENERATE(250, 750, 1750);
+
+            REQUIRE(dt2.toCyclicContinuous(i) == 0.5);
         }
 
         SECTION("toPeriodicContinuous") {
-            REQUIRE(dt1.toPeriodicContinuous(i) == 0.5);
+            auto [disc, cont] = GENERATE(std::make_tuple(0, 0.0), std::make_tuple(100, 0.2), std::make_tuple(250, 0.5), std::make_tuple(500, 1.0), std::make_tuple(750, 1.5), std::make_tuple(1000, 0.0), std::make_tuple(1250, 0.5), std::make_tuple(1500, 1.0), std::make_tuple(1750, 1.5));
+
+            REQUIRE(dt2.toPeriodicContinuous(disc) == cont);
+        }
+
+        SECTION("toCyclicDiscrete with small t") {
+            REQUIRE(dt2.toCyclicDiscrete(0.00001) == 0);
+        }
+
+        SECTION("toPeriodicDiscrete with small t") {
+            REQUIRE(dt2.toPeriodicDiscrete(0.00001) == 0);
+        }
+
+        SECTION("toDiscrete with many digits") {
+            REQUIRE(dt2.toDiscrete(0.99999999) == 499);
+        }
+
+        SECTION("toPeriodicDiscrete with many digits") {
+            REQUIRE(dt2.toPeriodicDiscrete(1.99999999) == 999);
+        }
+
+        SECTION("toPeriodicContinuous with many digits") {
+            REQUIRE(std::fabs(dt2.toPeriodicContinuous(999)-1.9999) > 0.0001);
         }
     }
 
-    SECTION("toDiscrete with small t") {
-        REQUIRE(dt1.toDiscrete(0.00001) == 0);
-    }
-
-    SECTION("toContinuous with 0") {
-        REQUIRE(dt1.toContinuous(0) == 0.0);
-    }
-
-    SECTION("toContinuous with 1") {
-        REQUIRE(dt1.toContinuous(1) > 0.0);
-    }
-
-}
-
-TEST_CASE( "Discrete time test 2" ) {
-    DiscreteTime dt2(500, 2.0);
-
-    SECTION("toDiscrete and toContinuous") {
-        auto [cont, disc] = GENERATE(std::make_tuple(0.5, 250), std::make_tuple(1.5, 750));
+    SECTION( "Discrete time test with f=500, periodScale=0.5" ) {
+        DiscreteTime dt3(500, 0.5);
         
-        SECTION("toDiscrete") {
-            REQUIRE(dt2.toDiscrete(cont) == disc);
+        SECTION("toDiscrete and toContinuous") {
+            auto [cont, disc] = GENERATE(std::make_tuple(0.5, 250), std::make_tuple(1.5, 750));
+            
+            SECTION("toDiscrete") {
+                REQUIRE(dt3.toDiscrete(cont) == disc);
+            }
+            
+            SECTION("toContinuous") {
+                REQUIRE(dt3.toContinuous(disc) == cont);
+            }
         }
-        
-        SECTION("toContinuous") {
-            REQUIRE(dt2.toContinuous(disc) == cont);
+
+        SECTION("toCyclicDiscrete") {
+            auto i = GENERATE(0.5, 1.5, 3.5);
+
+            REQUIRE(dt3.toCyclicDiscrete(i) == 250);
         }
-    }
 
-    SECTION("toCyclicDiscrete") {
-        auto i = GENERATE(0.5, 1.5, 3.5);
+        SECTION("toPeriodicDiscrete") {
+            auto [cont, disc] = GENERATE(std::make_tuple(0.0, 0), std::make_tuple(0.2, 100), std::make_tuple(0.5, 0), std::make_tuple(1.5, 0), std::make_tuple(2.0, 0), std::make_tuple(2.2, 100), std::make_tuple(2.5, 0), std::make_tuple(3.5, 0));
 
-        REQUIRE(dt2.toCyclicDiscrete(i) == 250);
-    }
-
-    SECTION("toPeriodicDiscrete") {
-        auto [cont, disc] = GENERATE(std::make_tuple(0.0, 0), std::make_tuple(0.2, 100), std::make_tuple(0.5, 250), std::make_tuple(1.5, 750), std::make_tuple(2.0, 0), std::make_tuple(2.2, 100), std::make_tuple(2.5, 250), std::make_tuple(3.5, 750));
-
-        REQUIRE(dt2.toPeriodicDiscrete(cont) == disc);
-    }
-
-    SECTION("toCyclicContinuous") {
-        auto i = GENERATE(250, 750, 1750);
-
-        REQUIRE(dt2.toCyclicContinuous(i) == 0.5);
-    }
-
-    SECTION("toPeriodicContinuous") {
-        auto [disc, cont] = GENERATE(std::make_tuple(0, 0.0), std::make_tuple(100, 0.2), std::make_tuple(250, 0.5), std::make_tuple(500, 1.0), std::make_tuple(750, 1.5), std::make_tuple(1000, 0.0), std::make_tuple(1250, 0.5), std::make_tuple(1500, 1.0), std::make_tuple(1750, 1.5));
-
-        REQUIRE(dt2.toPeriodicContinuous(disc) == cont);
-    }
-
-    SECTION("toCyclicDiscrete with small t") {
-        REQUIRE(dt2.toCyclicDiscrete(0.00001) == 0);
-    }
-
-    SECTION("toPeriodicDiscrete with small t") {
-        REQUIRE(dt2.toPeriodicDiscrete(0.00001) == 0);
-    }
-
-    SECTION("toDiscrete with many digits") {
-        REQUIRE(dt2.toDiscrete(0.99999999) == 499);
-    }
-
-    SECTION("toPeriodicDiscrete with many digits") {
-        REQUIRE(dt2.toPeriodicDiscrete(1.99999999) == 999);
-    }
-
-    SECTION("toPeriodicContinuous with many digits") {
-        REQUIRE(std::fabs(dt2.toPeriodicContinuous(999)-1.9999) > 0.0001);
-    }
-}
-
-TEST_CASE( "Discrete time test 3" ) {
-    DiscreteTime dt3(500, 0.5);
-    
-    SECTION("toDiscrete and toContinuous") {
-        auto [cont, disc] = GENERATE(std::make_tuple(0.5, 250), std::make_tuple(1.5, 750));
-        
-        SECTION("toDiscrete") {
-            REQUIRE(dt3.toDiscrete(cont) == disc);
+            REQUIRE(dt3.toPeriodicDiscrete(cont) == disc);
         }
-        
-        SECTION("toContinuous") {
-            REQUIRE(dt3.toContinuous(disc) == cont);
+
+        SECTION("toCyclicContinuous") {
+            auto [disc, cont] = GENERATE(std::make_tuple(250, 0.5), std::make_tuple(750, 0.5), std::make_tuple(1750, 0.5));
+
+            REQUIRE(dt3.toCyclicContinuous(disc) == cont);
+        }
+
+        SECTION("toPeriodicContinuous") {
+            auto [disc, cont] = GENERATE(std::make_tuple(0, 0.0), std::make_tuple(100, 0.2), std::make_tuple(250, 0.0), std::make_tuple(500, 0.0), std::make_tuple(750, 0.0), std::make_tuple(1000, 0.0), std::make_tuple(1250, 0.0), std::make_tuple(1350, 0.2), std::make_tuple(1500, 0.0), std::make_tuple(1750, 0.0));
+            
+            REQUIRE(dt3.toPeriodicContinuous(disc) == cont);
+        }
+
+        SECTION("toCyclicContinuous with 0") {
+            REQUIRE(dt3.toCyclicContinuous(0) == 0.0);
+        }
+
+        SECTION("toPeriodicContinuous with 1") {
+            REQUIRE(dt3.toPeriodicContinuous(1) > 0.0);
+        }
+
+        SECTION("toContinuous with many digits") {
+            REQUIRE(std::fabs(dt3.toContinuous(499)-0.999999) > 0.000001);
+        }
+
+        SECTION("toPeriodicDiscrete with many digits") {
+            REQUIRE(dt3.toPeriodicDiscrete(0.49999999) == 249);
+        }
+
+        SECTION("toPeriodicContinuous with many digits") {
+            REQUIRE(std::fabs(dt3.toPeriodicContinuous(249)-0.4999) > 0.0001);
         }
     }
 
-    SECTION("toCyclicDiscrete") {
-        auto i = GENERATE(0.5, 1.5, 3.5);
-
-        REQUIRE(dt3.toCyclicDiscrete(i) == 250);
+    SECTION( "Discrete time test with f=1000000, periodScale=1.0" ) {
+        DiscreteTime dt4(1000000, 1.0);
     }
 
-    SECTION("toPeriodicDiscrete") {
-        auto [cont, disc] = GENERATE(std::make_tuple(0.0, 0), std::make_tuple(0.2, 100), std::make_tuple(0.5, 0), std::make_tuple(1.5, 0), std::make_tuple(2.0, 0), std::make_tuple(2.2, 100), std::make_tuple(2.5, 0), std::make_tuple(3.5, 0));
-
-        REQUIRE(dt3.toPeriodicDiscrete(cont) == disc);
-    }
-
-    SECTION("toCyclicContinuous") {
-        auto [disc, cont] = GENERATE(std::make_tuple(250, 0.5), std::make_tuple(750, 0.5), std::make_tuple(1750, 0.5));
-
-        REQUIRE(dt3.toCyclicContinuous(disc) == cont);
-    }
-
-    SECTION("toPeriodicContinuous") {
-        auto [disc, cont] = GENERATE(std::make_tuple(0, 0.0), std::make_tuple(100, 0.2), std::make_tuple(250, 0.0), std::make_tuple(500, 0.0), std::make_tuple(750, 0.0), std::make_tuple(1000, 0.0), std::make_tuple(1250, 0.0), std::make_tuple(1350, 0.2), std::make_tuple(1500, 0.0), std::make_tuple(1750, 0.0));
-        
-        REQUIRE(dt3.toPeriodicContinuous(disc) == cont);
-    }
-
-    SECTION("toCyclicContinuous with 0") {
-        REQUIRE(dt3.toCyclicContinuous(0) == 0.0);
-    }
-
-    SECTION("toPeriodicContinuous with 1") {
-        REQUIRE(dt3.toPeriodicContinuous(1) > 0.0);
-    }
-
-    SECTION("toContinuous with many digits") {
-        REQUIRE(std::fabs(dt3.toContinuous(499)-0.999999) > 0.000001);
-    }
-
-    SECTION("toPeriodicDiscrete with many digits") {
-        REQUIRE(dt3.toPeriodicDiscrete(0.49999999) == 249);
-    }
-
-    SECTION("toPeriodicContinuous with many digits") {
-        REQUIRE(std::fabs(dt3.toPeriodicContinuous(249)-0.4999) > 0.0001);
-    }
-}
-
-TEST_CASE( "Discrete time test 4" ) {
-    DiscreteTime dt4(1000000, 1.0);
 }
