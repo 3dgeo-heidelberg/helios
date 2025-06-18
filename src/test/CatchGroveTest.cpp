f4a155da5e3ab698b42da913c33a3f1c3a8689c4
Felix <65565033+fexfl@users.noreply.github.com>
2025-06-18 15:49:58 +0200

    Removed unnecessary logging includes

--- src/test/CatchGroveTest.cpp
+++ src/test/CatchGroveTest.cpp
@@ -1,7 +1,4 @@
 #include <catch2/catch_test_macros.hpp>
-#undef WARN
-#undef INFO
-#include "logging.hpp"
 
 #include <KDGrove.h>
 #include <GroveKDTreeRaycaster.h>
--- src/test/CatchRigidMotionTest.cpp
+++ src/test/CatchRigidMotionTest.cpp
@@ -1,7 +1,4 @@
 #include <catch2/catch_test_macros.hpp>
-#undef WARN
-#undef INFO
-#include "logging.hpp"
 
 #include <MathConstants.h>
 #include <maths/rigidmotion/RigidMotionException.h>
