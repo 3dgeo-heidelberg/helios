76bf101204060ef8d84b583c5031007325ad1086
Felix <65565033+fexfl@users.noreply.github.com>
2025-06-27 13:39:27 +0200

    Fixed namespace in CatchRigidMotionTest

--- src/test/CatchRigidMotionTest.cpp
+++ src/test/CatchRigidMotionTest.cpp
@@ -22,30 +22,30 @@ TEST_CASE("Rigid motion test") {
         }
 
         SECTION("Identity R2") {
-            RigidMotion f = rm2f.makeIdentity();
+            rigidmotion::RigidMotion f = rm2f.makeIdentity();
             arma::mat Y = rme.apply(f, R2X);
             arma::mat Z = abs(Y-R2X);
             REQUIRE(!arma::any(arma::vectorise(Z) > eps));
-            REQUIRE(RigidMotion::SuperType::R2_BASE == f.findSuperType());
+            REQUIRE(rigidmotion::RigidMotion::SuperType::R2_BASE == f.findSuperType());
             REQUIRE(f.hasFixedPoints());
-            REQUIRE(RigidMotion::Type::IDENTITY_R2 == f.findType());
+            REQUIRE(rigidmotion::RigidMotion::Type::IDENTITY_R2 == f.findType());
             REQUIRE(f.findInvariantDimensionality() == 2);
             size_t dim;
             arma::mat L = rme.computeFixedPoints(f, dim);
-            arma::mat EL = eye(2, 2);
+            arma::mat EL = arma::eye(2, 2);
             REQUIRE(!arma::any(arma::vectorise(abs(L-EL)) > eps));
         }
 
         SECTION("Translation R2") {
-            colvec shift({1.67, -2.27});
-            RigidMotion f = rm2f.makeTranslation(shift);
+            arma::colvec shift({1.67, -2.27});
+            rigidmotion::RigidMotion f = rm2f.makeTranslation(shift);
             arma::mat Y = rme.apply(f, R2X);
             arma::mat EY = R2X.each_col() + shift;
             arma::mat Z = abs(Y-EY);
             REQUIRE(!arma::any(arma::vectorise(Z) > eps));
-            REQUIRE(RigidMotion::SuperType::R2_BASE == f.findSuperType());
+            REQUIRE(rigidmotion::RigidMotion::SuperType::R2_BASE == f.findSuperType());
             REQUIRE(!f.hasFixedPoints());
-            REQUIRE(RigidMotion::Type::TRANSLATION_R2 == f.findType());
+            REQUIRE(rigidmotion::RigidMotion::Type::TRANSLATION_R2 == f.findType());
             REQUIRE(f.findInvariantDimensionality() == 2);
             try {
                 size_t dim;
@@ -57,15 +57,15 @@ TEST_CASE("Rigid motion test") {
         }
 
         SECTION("Reflection R2") {
-            colvec axis({2.1, -3.9});
-            colvec X({-3, 1});
-            RigidMotion f = rm2f.makeReflection(axis);
-            colvec Y = rme.apply(f, X);
-            colvec EY({0.81651376, 3.05504587});
+            arma::colvec axis({2.1, -3.9});
+            arma::colvec X({-3, 1});
+            rigidmotion::RigidMotion f = rm2f.makeReflection(axis);
+            arma::colvec Y = rme.apply(f, X);
+            arma::colvec EY({0.81651376, 3.05504587});
             REQUIRE(!arma::any(arma::vectorise(abs(Y-EY)) > eps));
-            REQUIRE(RigidMotion::SuperType::R2_REFLECTION == f.findSuperType());
+            REQUIRE(rigidmotion::RigidMotion::SuperType::R2_REFLECTION == f.findSuperType());
             REQUIRE(f.hasFixedPoints());
-            REQUIRE(RigidMotion::Type::REFLECTION_R2 == f.findType());
+            REQUIRE(rigidmotion::RigidMotion::Type::REFLECTION_R2 == f.findType());
             REQUIRE(f.findInvariantDimensionality() == 1);
             size_t dim;
             arma::mat L = rme.computeFixedPoints(f, dim);
@@ -97,8 +97,8 @@ TEST_CASE("Rigid motion test") {
         }
 
         SECTION("Glide Reflection R2") {
-            colvec axis({2.1, 1.9});
-            RigidMotion f = rm2f.makeGlideReflection(axis, 2);
+            arma::colvec axis({2.1, 1.9});
+            rigidmotion::RigidMotion f = rm2f.makeGlideReflection(axis, 2);
             arma::mat Y = rme.apply(f, R2X);
             arma::mat EY(2, 5);
             EY.at(0, 0) = 2.7262137;        EY.at(1, 0) = -1.29533046;
@@ -108,9 +108,9 @@ TEST_CASE("Rigid motion test") {
             EY.at(0, 4) = -4.43588106;      EY.at(1, 4) = 7.46277428;
             arma::mat Z = abs(Y-EY);
             REQUIRE(!arma::any(arma::vectorise(Z) > eps));
-            REQUIRE(RigidMotion::SuperType::R2_REFLECTION == f.findSuperType());
+            REQUIRE(rigidmotion::RigidMotion::SuperType::R2_REFLECTION == f.findSuperType());
             REQUIRE(!f.hasFixedPoints());
-            REQUIRE(RigidMotion::Type::GLIDE_REFLECTION_R2 == f.findType());
+            REQUIRE(rigidmotion::RigidMotion::Type::GLIDE_REFLECTION_R2 == f.findType());
             REQUIRE(f.findInvariantDimensionality() == 1);
             size_t dim;
             arma::mat L = rme.computeAssociatedInvariant(f, dim);
@@ -119,16 +119,16 @@ TEST_CASE("Rigid motion test") {
             REQUIRE(!arma::any(arma::vectorise(abs(L-EL)) > eps));
 
             f = rm2f.makeTranslation(2*axis/norm(axis));
-            RigidMotion g = rm2f.makeReflection(axis);
-            RigidMotion h = rme.compose(f, g);
+            rigidmotion::RigidMotion g = rm2f.makeReflection(axis);
+            rigidmotion::RigidMotion h = rme.compose(f, g);
             arma::mat Y2 = rme.apply(h, R2X);
             Z = abs(Y-Y2);
             REQUIRE(!arma::any(arma::vectorise(Z) > eps));
         }
 
         SECTION("Rotation R2") {
-            colvec center = R2X.col(2);
-            RigidMotion f = rm2f.makeRotation(-M_PI/3, center);
+            arma::colvec center = R2X.col(2);
+            rigidmotion::RigidMotion f = rm2f.makeRotation(-M_PI/3, center);
             arma::mat Y = rme.apply(f, R2X);
             arma::mat EY(2, 5);
             EY.at(0, 0) = 2.96410162;       EY.at(1, 0) = 2.96410162;
@@ -138,9 +138,9 @@ TEST_CASE("Rigid motion test") {
             EY.at(0, 4) = 0.03589838;       EY.at(1, 4) = -7.96410162;
             arma::mat Z = abs(Y-EY);
             REQUIRE(!arma::any(arma::vectorise(Z) > eps));
-            REQUIRE(RigidMotion::SuperType::R2_ROTATION == f.findSuperType());
+            REQUIRE(rigidmotion::RigidMotion::SuperType::R2_ROTATION == f.findSuperType());
             REQUIRE(f.hasFixedPoints());
-            REQUIRE(RigidMotion::Type::ROTATION_R2 == f.findType());
+            REQUIRE(rigidmotion::RigidMotion::Type::ROTATION_R2 == f.findType());
             REQUIRE(f.findInvariantDimensionality() == 0);
             size_t dim;
             arma::mat L = rme.computeFixedPoints(f, dim);
@@ -150,21 +150,21 @@ TEST_CASE("Rigid motion test") {
     }
 
     SECTION("RigidMotion: Pure R3 Motions") {
-        RigidMotionEngine rme;
-        RigidMotionR3Factory rm3f;
+        rigidmotion::RigidMotionEngine rme;
+        rigidmotion::RigidMotionR3Factory rm3f;
         arma::mat R3X(3, 5);
         for(size_t i = 0 ; i < 15 ; ++i){
             R3X.at(i) = (((double)i)-2.5)*std::pow(-1.0, (double)i);
         }
 
         SECTION("Identity R3") {
-            RigidMotion f = rm3f.makeIdentity();
+            rigidmotion::RigidMotion f = rm3f.makeIdentity();
             arma::mat Y = rme.apply(f, R3X);
             arma::mat Z = abs(Y-R3X);
             REQUIRE(!arma::any(arma::vectorise(Z) > eps));
-            REQUIRE(RigidMotion::SuperType::R3_BASE == f.findSuperType());
+            REQUIRE(rigidmotion::RigidMotion::SuperType::R3_BASE == f.findSuperType());
             REQUIRE(f.hasFixedPoints());
-            REQUIRE(RigidMotion::Type::IDENTITY_R3 == f.findType());
+            REQUIRE(rigidmotion::RigidMotion::Type::IDENTITY_R3 == f.findType());
             REQUIRE(f.findInvariantDimensionality() == 3);
             size_t dim;
             arma::mat L = rme.computeFixedPoints(f, dim);
@@ -177,21 +177,21 @@ TEST_CASE("Rigid motion test") {
         }
 
         SECTION("Translation R3") {
-            colvec shift({1.1, -3.39, 0.24});
-            RigidMotion f = rm3f.makeTranslation(shift);
+            arma::colvec shift({1.1, -3.39, 0.24});
+            rigidmotion::RigidMotion f = rm3f.makeTranslation(shift);
             arma::mat Y = rme.apply(f, R3X);
             arma::mat EY = R3X.each_col() + shift;
             arma::mat Z = abs(Y-EY);
             REQUIRE(!arma::any(arma::vectorise(Z) > eps));
-            REQUIRE(RigidMotion::SuperType::R3_BASE == f.findSuperType());
+            REQUIRE(rigidmotion::RigidMotion::SuperType::R3_BASE == f.findSuperType());
             REQUIRE(!f.hasFixedPoints());
-            REQUIRE(RigidMotion::Type::TRANSLATION_R3 == f.findType());
+            REQUIRE(rigidmotion::RigidMotion::Type::TRANSLATION_R3 == f.findType());
             REQUIRE(f.findInvariantDimensionality() == 3);
         }
 
         SECTION("Reflection R3") {
-            colvec ortho({1.0, 0.5, 0.3});
-            RigidMotion f = rm3f.makeReflection(ortho);
+            arma::colvec ortho({1.0, 0.5, 0.3});
+            rigidmotion::RigidMotion f = rm3f.makeReflection(ortho);
             arma::mat Y = rme.apply(f, R3X);
             arma::mat EY(
                 "0.3358209 0.24626866 -0.82835821 1.41044776 -1.99253731; "
@@ -203,9 +203,9 @@ TEST_CASE("Rigid motion test") {
             Y = rme.apply(f, Y);
             Z = abs(Y-R3X);
             REQUIRE(!arma::any(arma::vectorise(Z) > eps));
-            REQUIRE(RigidMotion::SuperType::R3_REFLECTION == f.findSuperType());
+            REQUIRE(rigidmotion::RigidMotion::SuperType::R3_REFLECTION == f.findSuperType());
             REQUIRE(f.hasFixedPoints());
-            REQUIRE(RigidMotion::Type::REFLECTION_R3 == f.findType());
+            REQUIRE(rigidmotion::RigidMotion::Type::REFLECTION_R3 == f.findType());
             REQUIRE(f.findInvariantDimensionality() == 2);
             size_t dim;
             arma::mat L = rme.computeFixedPoints(f, dim);
@@ -215,9 +215,9 @@ TEST_CASE("Rigid motion test") {
         }
 
         SECTION("Glide Reflection R3") {
-            colvec ortho({1.0, 0.5, 0.3});
-            colvec shift({-4.0/5.0, 1, 1});
-            RigidMotion f = rm3f.makeGlideReflection(ortho, shift);
+            arma::colvec ortho({1.0, 0.5, 0.3});
+            arma::colvec shift({-4.0/5.0, 1, 1});
+            rigidmotion::RigidMotion f = rm3f.makeGlideReflection(ortho, shift);
             arma::mat Y = rme.apply(f, R3X);
             arma::mat EY = (
                 "-0.4641791   -0.55373134  -1.62835821   0.61044776  -2.79253731;"
@@ -226,9 +226,9 @@ TEST_CASE("Rigid motion test") {
             );
             arma::mat Z = abs(Y-EY);
             REQUIRE(!arma::any(arma::vectorise(Z) > eps));
-            REQUIRE(RigidMotion::SuperType::R3_REFLECTION == f.findSuperType());
+            REQUIRE(rigidmotion::RigidMotion::SuperType::R3_REFLECTION == f.findSuperType());
             REQUIRE(!f.hasFixedPoints());
-            REQUIRE(RigidMotion::Type::GLIDE_REFLECTION_R3 == f.findType());
+            REQUIRE(rigidmotion::RigidMotion::Type::GLIDE_REFLECTION_R3 == f.findType());
             REQUIRE(f.findInvariantDimensionality() == 2);
             size_t dim;
             arma::mat L = rme.computeAssociatedInvariant(f, dim);
@@ -240,7 +240,7 @@ TEST_CASE("Rigid motion test") {
             Z = abs(Y-R3X);
             REQUIRE(!arma::any(arma::vectorise(Z) > eps));
             try {
-                f = rm3f.makeGlideReflection(ortho, colvec("1 1 1"));
+                f = rm3f.makeGlideReflection(ortho, arma::colvec("1 1 1"));
                 FAIL("Should have thrown RigidMotionException");
             } catch (rigidmotion::RigidMotionException&) {
                 SUCCEED();
@@ -248,9 +248,9 @@ TEST_CASE("Rigid motion test") {
         }
 
         SECTION("Rotation R3") {
-            colvec axis({0.4, 0.2, 1.0});
+            arma::colvec axis({0.4, 0.2, 1.0});
             double theta = 4.5;
-            RigidMotion f = rm3f.makeRotation(axis, theta);
+            rigidmotion::RigidMotion f = rm3f.makeRotation(axis, theta);
             arma::mat Y = rme.apply(f, R3X);
             arma::mat EY(
                 "1.4704453    0.92147891  -3.31340313   5.70532734  -8.09725156;"
@@ -259,9 +259,9 @@ TEST_CASE("Rigid motion test") {
             );
             arma::mat Z = abs(Y-EY);
             REQUIRE(!arma::any(arma::vectorise(Z) > eps));
-            REQUIRE(RigidMotion::SuperType::R3_ROTATION == f.findSuperType());
+            REQUIRE(rigidmotion::RigidMotion::SuperType::R3_ROTATION == f.findSuperType());
             REQUIRE(f.hasFixedPoints());
-            REQUIRE(RigidMotion::Type::ROTATION_R3 == f.findType());
+            REQUIRE(rigidmotion::RigidMotion::Type::ROTATION_R3 == f.findType());
             REQUIRE(f.findInvariantDimensionality() == 1);
             size_t dim;
             arma::mat L = rme.computeFixedPoints(f, dim);
@@ -276,8 +276,8 @@ TEST_CASE("Rigid motion test") {
     }
 
     SECTION("RigidMotion: Helios Dynamic Motions") {
-        RigidMotionEngine rme;
-        RigidMotionR3Factory rm3f;
+        rigidmotion::RigidMotionEngine rme;
+        rigidmotion::RigidMotionR3Factory rm3f;
         DynMotionEngine dme;
 
         // Build dynamic moving object for the tests
@@ -297,7 +297,7 @@ TEST_CASE("Rigid motion test") {
         DynMovingObject dmo("HRMTestDMO", primitives);
         dmo.computeCentroid();
 
-        auto validate_dyn_motion = [&](const DynMotion& dm, const RigidMotion& rm, DynMovingObject dynObj) {
+        auto validate_dyn_motion = [&](const DynMotion& dm, const rigidmotion::RigidMotion& rm, DynMovingObject dynObj) {
             arma::mat X = dynObj.positionMatrixFromPrimitives();
             arma::mat dmeY = dme.apply(dm, X, dynObj);
             arma::mat rmeY = rme.apply(rm, X);
@@ -309,14 +309,14 @@ TEST_CASE("Rigid motion test") {
 
         SECTION("Identity R3") {
             DynMovingObject dynObj = dmo;
-            RigidMotion id = rm3f.makeIdentity();
+            rigidmotion::RigidMotion id = rm3f.makeIdentity();
             DynMotion dm(id, false);
             validate_dyn_motion(dm, id, dynObj);
         }
 
         SECTION("Translation R3") {
             DynMovingObject dynObj = dmo;
-            RigidMotion tr = rm3f.makeTranslation(arma::colvec("-0.1;0.1;0"));
+            rigidmotion::RigidMotion tr = rm3f.makeTranslation(arma::colvec("-0.1;0.1;0"));
             DynMotion dm(tr, false);
             validate_dyn_motion(dm, tr, dynObj);
         }
@@ -324,7 +324,7 @@ TEST_CASE("Rigid motion test") {
         SECTION("Reflection R3") {
             DynMovingObject dynObj = dmo;
             arma::colvec O = dmo.getCentroid();
-            RigidMotion rf = rme.compose(
+            rigidmotion::RigidMotion rf = rme.compose(
                 rm3f.makeTranslation(O),
                 rme.compose(rm3f.makeReflectionZ(), rm3f.makeTranslation(-O))
             );
@@ -335,7 +335,7 @@ TEST_CASE("Rigid motion test") {
         SECTION("Glide Reflection R3") {
             DynMovingObject dynObj = dmo;
             arma::colvec O = dmo.getCentroid();
-            RigidMotion rf = rme.compose(
+            rigidmotion::RigidMotion rf = rme.compose(
                 rm3f.makeTranslation(O),
                 rme.compose(
                     rm3f.makeGlideReflection(
@@ -358,7 +358,7 @@ TEST_CASE("Rigid motion test") {
         SECTION("Rotation R3") {
             DynMovingObject dynObj = dmo;
             arma::colvec O = dmo.getCentroid();
-            RigidMotion rot = rme.compose(
+            rigidmotion::RigidMotion rot = rme.compose(
                 rm3f.makeTranslation(O),
                 rme.compose(rm3f.makeRotationZ(0.67), rm3f.makeTranslation(-O))
             );
@@ -369,7 +369,7 @@ TEST_CASE("Rigid motion test") {
         SECTION("Helical R3") {
             DynMovingObject dynObj = dmo;
             arma::colvec O = dmo.getCentroid();
-            RigidMotion hm = rme.compose(
+            rigidmotion::RigidMotion hm = rme.compose(
                 rm3f.makeTranslation(O),
                 rme.compose(rm3f.makeHelicalZ(0.5, 1.0), rm3f.makeTranslation(-O))
             );
@@ -380,7 +380,7 @@ TEST_CASE("Rigid motion test") {
         SECTION("Rotational Symmetry R3") {
             DynMovingObject dynObj = dmo;
             arma::colvec O = dmo.getCentroid();
-            RigidMotion rs = rme.compose(
+            rigidmotion::RigidMotion rs = rme.compose(
                 rm3f.makeTranslation(O),
                 rme.compose(
                     rm3f.makeRotationalSymmetryZ(0.34),
