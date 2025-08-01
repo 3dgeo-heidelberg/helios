844476a62ceb06f0a0f0926d696eecdff30ba1f4
DimasfromLavoisier <madgfess@gmail.com>
2025-08-01 15:50:56 +0200

    add serial_id set for proper work

--- python/helios/utils.py
+++ python/helios/utils.py
@@ -266,6 +266,10 @@ def apply_scene_shift(
     If this survey was not loaded from XML, apply `make_scene_shift` once.
     Subsequent calls are no-ops.
     """
+    
+    # we need to add serial IDs to the legs for proper process of writing into the file
+    for i, leg in enumerate(survey.legs):
+        leg._cpp_object.serial_id = i
 
     if getattr(survey, "_scene_shift_done", False):
         return
