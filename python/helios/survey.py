76f8c6b6b51d728545922dd8a0fa508ba2aa70e9
DimasfromLavoisier <madgfess@gmail.com>
2025-08-05 09:25:35 +0200

    code pieces required for applying scene shift

--- python/helios/survey.py
+++ python/helios/survey.py
@@ -73,9 +73,11 @@ class Survey(Model, cpp_class=_helios.Survey):
         # Apply shift once and only if the survey is not loaded from XML
         if not is_xml_loaded(self):
             apply_scene_shift(self, execution_settings)
-
+        
         # Ensure that the scene has been finalized
-        self.scene._finalize(execution_settings)
+        if not is_xml_loaded(self):
+            self.scene._finalize(execution_settings)
+        
         self.scene._set_reflectances(self.scanner._cpp_object.wavelength)
 
         # Set the fullwave form settings on the scanner
@@ -188,6 +190,7 @@ class Survey(Model, cpp_class=_helios.Survey):
         # Return path to the created output directory
         return Path(playback.fms.write.get_measurement_writer_output_path()).parent
 
+
     def add_leg(
         self,
         leg: Optional[Leg] = None,
@@ -202,8 +205,14 @@ class Survey(Model, cpp_class=_helios.Survey):
         from the provided settings.
         """
 
-        copy_platform_settings = PlatformSettings()
-        copy_scanner_settings = ScannerSettings()
+        copy_platform_settings = (
+            platform_settings.__class__() if platform_settings is not None
+            else PlatformSettings()
+        )
+        copy_scanner_settings = (
+            scanner_settings.__class__() if scanner_settings is not None
+            else ScannerSettings()
+        )
         # Set the parameters given as scanner + platform settings
         if platform_settings is not None:
             copy_platform_settings.update_from_object(platform_settings)
@@ -233,7 +242,6 @@ class Survey(Model, cpp_class=_helios.Survey):
         # Update with the rest of the given parameters
         leg.platform_settings.update_from_dict(parameters, skip_exceptions=True)
         leg.scanner_settings.update_from_dict(parameters, skip_exceptions=True)
-
         # If there are parameters left, we raise an error
         if parameters:
             raise ValueError(f"Unknown parameters: {', '.join(parameters)}")
