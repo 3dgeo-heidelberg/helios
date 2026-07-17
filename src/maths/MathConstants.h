b7cf00d4222f5b2ada77e4a55cfdb484ba64ad9c
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2026-07-17 10:08:53 +0200

    Make all math constants constexpr

--- src/maths/MathConstants.h
+++ src/maths/MathConstants.h
@@ -17,77 +17,77 @@ constexpr double EULER_NUMBER = 2.71828182845904523536;
  *  \frac{\pi}{2}
  * \f]
  */
-const double PI_HALF = PI / 2.0;
+constexpr double PI_HALF = PI / 2.0;
 
 /**
  * \f[
  *  \frac{\pi}{4}
  * \f]
  */
-const double PI_QUARTER = PI / 4.0;
+constexpr double PI_QUARTER = PI / 4.0;
 /**
  * \frac[
  *  \frac{\pi}{8}
  * \f]
  */
-const double PI_EIGHTH = PI / 8.0;
+constexpr double PI_EIGHTH = PI / 8.0;
 
 /**
  * \f[
  *  \frac{3{\pi}}{2}
  * \f]
  */
-const double PI_3_HALF = PI_HALF * 3.0;
+constexpr double PI_3_HALF = PI_HALF * 3.0;
 
 /**
  * \f[
  *  2{\pi}
  * \f]
  */
-const double PI_2 = PI * 2.0;
+constexpr double PI_2 = PI * 2.0;
 
 /**
  * \f[
  *  4{\pi}
  * \f]
  */
-const double PI_4 = PI * 4.0;
+constexpr double PI_4 = PI * 4.0;
 /**
  * \f[
  *  \pi^2
  * \f]
  */
-const double PI_SQUARED = PI * PI;
+constexpr double PI_SQUARED = PI * PI;
 
 /**
  * \f[
  *  2 \pi^2
  * \f]
  */
-const double PI_SQUARED_2 = 2 * PI * PI;
+constexpr double PI_SQUARED_2 = 2 * PI * PI;
 
 /**
  * \f[
  *  \frac{180}{\pi}
  * \f]
  */
-const double _180_OVER_PI = 180.0 / PI;
+constexpr double _180_OVER_PI = 180.0 / PI;
 
 /**
  * \f[
  *  \frac{\pi}{180}
  * \f]
  */
-const double PI_OVER_180 = PI / 180.0;
+constexpr double PI_OVER_180 = PI / 180.0;
 
 /**
  * @brief Constant representing a very close from above to -1 number
  */
-const double ALMOST_MINUS_1 = -0.9999999999;
+constexpr double ALMOST_MINUS_1 = -0.9999999999;
 /**
  * @brief Constant representing a very close from below to 1 number
  */
-const double ALMOST_PLUS_1 = 0.9999999999;
+constexpr double ALMOST_PLUS_1 = 0.9999999999;
 
 /**
  * \f[
@@ -95,19 +95,19 @@ const double ALMOST_PLUS_1 = 0.9999999999;
  * \f]
  */
 
-const double SQRT2 = 1.4142135623730951;
+constexpr double SQRT2 = 1.4142135623730951;
 
 /**
  * @brief Speed of light in meters per second
  */
-const double SPEEDofLIGHT_mPerSec = 299792458;
+constexpr double SPEEDofLIGHT_mPerSec = 299792458;
 
 /**
  * @brief Speed of light in meters per nanosecond
  */
-const double SPEEDofLIGHT_mPerNanosec = 0.299792458;
+constexpr double SPEEDofLIGHT_mPerNanosec = 0.299792458;
 
 /**
  * @brief Speed of light in meters per picosecond
  */
-const double SPEEDofLIGHT_mPerPicosec = 0.000299792458;
+constexpr double SPEEDofLIGHT_mPerPicosec = 0.000299792458;
