c34d22286811c8c0e4077c5b1f725ff686d31f94
han16nah <han_nah@mailbox.org>
2026-09-03 23:40:05 +0200

    replace outdated "get_bbox()" function (used before the bbox property was exposed in Python) by np.array(part.bbox.bounds)

--- tests/python/test_scene.py
+++ tests/python/test_scene.py
@@ -529,11 +529,6 @@ def test_scenepart_from_vox():
         )
 
 
-def get_bbox(part):
-    scene = StaticScene(scene_parts=[part])
-    return np.array(scene._cpp_object.bbox_crs.bounds)
-
-
 def test_scenepart_bbox_extraction(box):
     bbox = box.bbox
     assert isinstance(bbox, BoundingBox)
@@ -574,13 +569,13 @@ def test_scenepart_bbox_updates_after_transformations(box):
 
 
 def check_rotation(box1, box2):
-    bbox1 = get_bbox(box1)
+    bbox1 = np.array(box1.bbox.bounds)
     bbox1[0][1] = bbox1[0][1] * math.sqrt(2)
     bbox1[0][2] = bbox1[0][2] * math.sqrt(2)
     bbox1[1][1] = bbox1[1][1] * math.sqrt(2)
     bbox1[1][2] = bbox1[1][2] * math.sqrt(2)
 
-    bbox2 = get_bbox(box2)
+    bbox2 = np.array(box2.bbox.bounds)
     assert np.allclose(bbox1, bbox2)
 
 
@@ -647,8 +642,8 @@ def test_rotate_scenepart_rotation_center(box_f):
 
     box1.rotate(angle="180 deg", axis=[0, 0, 1.0], rotation_center=[100.0, 0.0, 0.0])
     box2.translate([200.0, 0.0, 0.0])
-    bbox1 = get_bbox(box1)
-    bbox2 = get_bbox(box2)
+    bbox1 =  np.array(box1.bbox.bounds)
+    bbox2 =  np.array(box2.bbox.bounds)
 
     assert np.allclose(bbox1, bbox2)
 
@@ -660,8 +655,8 @@ def test_scale_mesh_scenepart(box_f):
     scale = 2.0
     box2.scale(scale)
 
-    bbox1 = get_bbox(box1)
-    bbox2 = get_bbox(box2)
+    bbox1 = np.array(box1.bbox.bounds)
+    bbox2 = np.array(box2.bbox.bounds)
 
     assert np.allclose(bbox1 * scale, bbox2)
 
@@ -702,8 +697,8 @@ def test_transform_scenepart(box_f):
     offset = np.array([10.0, 0, 0])
     box2.translate(offset)
 
-    bbox1 = get_bbox(box1)
-    bbox2 = get_bbox(box2)
+    bbox1 = np.array(box1.bbox.bounds)
+    bbox2 = np.array(box2.bbox.bounds)
 
     assert np.allclose(bbox1 + offset, bbox2)
 
